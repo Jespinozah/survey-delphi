a6a4b97d3e87fcbb80eefda3e3f0db2d8faed165
jespinozah <bjespinozah@gmail.com>
2017-06-13 16:09:50 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+export class App {
+  configureRouter(config, router) {
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
+      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
+      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' },
+      { route: 'delphi-project',  name: 'delphi-project', moduleId: 'delphi-project', nav: true, title: 'Delphi Projects' }
+    ]);
+
+    this.router = router;
+  }
+}
---
+++ src/delphi-project.js
@@ -0,0 +1,5 @@
+export class DelphiProject {
+  constructor() {
+    this.message = "DelphiProject";
+  }
+}
